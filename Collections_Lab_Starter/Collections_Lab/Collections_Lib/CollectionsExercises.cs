cedda177c0320645088c3bb87d7b6c59273264f6
Ned-57 <nedtelfer@gmail.com>
2022-03-11 10:51:56 +0000

    initial commit

---
+++ Collections_Lab_Starter/Collections_Lab/Collections_Lib/CollectionsExercises.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace Collections_Lib
+{
+    public class CollectionsExercises
+    {
+
+        /* removes and returns the next num entries in the queue, as a comma separated string */
+        public static string NextInQueue(int num, Queue<string> queue)
+        {
+            string commaSeparated = "";
+            if (queue.Count < num)
+            {
+                return commaSeparated;
+            }
+            for (int i = 0; i < num - 1; i++)
+            {
+                commaSeparated += queue.Dequeue() + ", ";
+            }
+            if (num == 0)
+                return commaSeparated;
+            else
+            {
+                commaSeparated += queue.Dequeue();
+                return commaSeparated;
+            }
+        }
+
+        /* uses a Stack to create and return array of ints in reverse order to the one supplied */
+        public static int[] Reverse(int[] original)
+        {
+            int[] reversed = new int[original.Length];
+            Stack<int> stack = new Stack<int>();
+            foreach (int n in original)
+            {
+                stack.Push(n);
+            }
+            for (int i = 0; i < original.Length; i++)
+            {
+                reversed[i] = stack.Pop();
+            }
+            return reversed;
+        }
+        // using a Dictionary, counts and returns (as a string) the occurence of the digits 0-9 in the given string
+        public static string CountDigits(string input)
+        {
+            Dictionary<char, int> digitCounter = new Dictionary<char, int>();
+            foreach (char c in input)
+            {
+                if (digitCounter.ContainsKey(c))
+                {
+                    digitCounter[c]++;
+                }
+                else if (c == '9' || c == '8' || c == '7' || c == '6' || c == '5' || c == '4' || c == '3' || c == '2' || c == '1' || c == '0')
+                {
+                    digitCounter.Add(c, 1);
+                }
+
+            }
+            string digitCountList = "";
+            foreach (KeyValuePair<char, int> keyValuePair in digitCounter)
+            {
+                digitCountList += keyValuePair;
+            }
+
+            return digitCountList;
+
+
+        }
+    }
+}
---
+++ Collections_Lab_Starter/Collections_Lab/Collections_Lib/ListExercises.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Collections_Lib
+{
+    public class ListExercises
+    {
+        // returns a list of all the integers between 1 to max inclusive
+        // that are multiples of 5
+        public static List<int> MakeFiveList(int max)
+        {
+            List<int> list = new List<int>();
+            for (int i = 1; i < max + 1; i++)
+            {
+                if (i % 5 == 0)
+                    list.Add(i);
+            }
+            return list;
+        }
+
+        // returns a list of all the strings in sourceList that start with the letter 'A' or 'a'
+        public static List<string> MakeAList(List<string> sourceList)
+        {
+            List<string> aList = new List<string>();
+            foreach (var item in sourceList)
+            {
+                if (item[0] == 'A' || item[0] == 'a')
+                {
+                    aList.Add(item);
+                }
+            };
+            return aList;
+        }
+    }
+}
